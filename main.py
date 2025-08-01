3ec30c1ae09caf03d7a985ab3672e33b0f5babf5
Abhi Tanwar <singhabhimanyu186@gmail.com>
2025-08-01 20:40:16 +0530

    data updates

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ServerConfig.CORS_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
